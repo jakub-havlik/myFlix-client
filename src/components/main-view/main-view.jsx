0306865cbd205ef3723039336369521003032206
jakub-havlik <havlik9@gmail.com>
2022-03-24 11:36:31 +0100

    updated main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -9,12 +9,15 @@ export class MainView extends React.Component {
     super();
     this.state = {
       movies: [
-        { _id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: '...'},
-        { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: '...'},
-        { _id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: '...'}
+        { _id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: 'https://flxt.tmsimg.com/assets/p7825626_p_v8_af.jpg'},
+        { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: 'https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_FMjpg_UX1000_.jpg'},
+        { _id: 3, Title: 'The Godfather', Description: 'desc3...', ImagePath: 'https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg'},
+        { _id: 4, Title: 'The Dark Knight', Description: 'desc4...', ImagePath: 'https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg'},
+        { _id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: 'https://m.media-amazon.com/images/M/MV5BMDliMmNhNDEtODUyOS00MjNlLTgxODEtN2U3NzIxMGVkZTA1L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg'}
       ],
       selectedMovie: null
     }
+
   }
 
   setSelectedMovie(newSelectedMovie) {
