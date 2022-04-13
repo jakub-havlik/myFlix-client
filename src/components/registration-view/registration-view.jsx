0261ee27de661bd1fa040ac08bf35fa3437e10ae
jakub-havlik <havlik9@gmail.com>
2022-04-13 11:54:51 +0200

    updated registration-view.jsx

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 
+<<<<<<< Updated upstream
 export function RegisterView(props) {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
@@ -45,4 +46,59 @@ export function RegisterView(props) {
             <button type="submit" onClick={handleSubmit}>Register</button>
         </form>
     );
+=======
+export function RegistrationView(props) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [birthday, setBirthday] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, password, email, birthday);
+    /* Send a request to the server for authentication */
+    /* then call props.onLoggedIn(username) */
+    props.onRegistration(username);
+  };
+
+
+  return (
+    <form>
+      <p>
+        <label>
+          Username:
+          <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+        </label>
+      </p>
+      <p>
+        <label>
+          Password:
+          <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+        </label>
+      </p>
+      <p>
+        <label>
+          Repeat Password:
+          <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+        </label>
+      </p>
+      <p>
+        <label>
+          Email:
+          <input type="email" value={password} onChange={e => setEmail(e.target.value)} />
+        </label>
+      </p>
+      <p>
+        <label>
+          Birthday:
+          <input type="birthday" value={password} onChange={e => setBirthday(e.target.value)} />
+        </label>
+      </p>
+
+      <button type="submit" onClick={handleSubmit}>Register</button>
+    </form>
+  );
+
+
+>>>>>>> Stashed changes
 }
