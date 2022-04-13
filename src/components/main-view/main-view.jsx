97351de3d3ed8802222e48100937960c3276bc8c
jakub-havlik <havlik9@gmail.com>
2022-04-13 14:19:37 +0200

    updated main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,35 +1,13 @@
-<<<<<<< Updated upstream
-import React from 'react';
-import axios from 'axios';
-
-=======
 // main-view is the root component
 
 import React from 'react';
 import axios from 'axios';
 
 import { RegistrationView } from '../registration-view/registration-view';
->>>>>>> Stashed changes
 import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
-<<<<<<< Updated upstream
-export class MainView extends React.Component {
-
-  constructor() {
-     super();
- // Initial state is set to null
-     this.state = {
-       movies: [],
-       selectedMovie: null,
-       user: null
-     };
-   }
-
-
-  componentDidMount(){
-=======
 
 export class MainView extends React.Component {
 
@@ -45,7 +23,6 @@ export class MainView extends React.Component {
 
 
   componentDidMount() {
->>>>>>> Stashed changes
     axios.get('https://listapeli.herokuapp.com/movies')
       .then(response => {
         this.setState({
@@ -57,10 +34,6 @@ export class MainView extends React.Component {
       });
   }
 
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
   /*When a movie is clicked, this function is invoked and updates the state of the `selectedMovie` *property to that movie*/
 
   setSelectedMovie(movie) {
@@ -69,12 +42,7 @@ export class MainView extends React.Component {
     });
   }
 
-<<<<<<< Updated upstream
-
-/* When a user successfully logs in, this function updates the `user` property in state to that *particular user*/
-=======
   /* When a user successfully logs in, this function updates the `user` property in state to that *particular user*/
->>>>>>> Stashed changes
 
   onLoggedIn(user) {
     this.setState({
@@ -96,30 +64,16 @@ export class MainView extends React.Component {
       <div className="main-view">
         {/*If the state of `selectedMovie` is not null, that selected movie will be returned otherwise, all *movies will be returned*/}
         {selectedMovie
-<<<<<<< Updated upstream
-          ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
-          : movies.map(movie => (
-            <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
-         ))
-=======
           ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
           : movies.map(movie => (
             <MovieCard key={movie._id} movieData={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }} />
           ))
->>>>>>> Stashed changes
         }
       </div>
     );
   }
 
 
-<<<<<<< Updated upstream
-
-}
-
-
-export default MainView;
-=======
 }
 
 
@@ -135,4 +89,3 @@ export default MainView;
 
 
 
->>>>>>> Stashed changes
