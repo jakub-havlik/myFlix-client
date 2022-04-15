39c6a28b72e1b4796b37c61e8adf274530aeb133
jakub-havlik <havlik9@gmail.com>
2022-04-15 09:45:51 +0200

    updated login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,10 +1,17 @@
 import React, { useState } from 'react';
+<<<<<<< Updated upstream
+=======
+import axios from 'axios';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+>>>>>>> Stashed changes
 
 export function LoginView(props) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
 
+<<<<<<< Updated upstream
   /* In the login screen you should make the request to authenticate the user and then save the token to be used in future requests*/
 
   const handleSubmit = async (e) => {
@@ -40,8 +47,51 @@ export function LoginView(props) {
       <button type="submit" onClick={handleSubmit}>Login</button>
       <button onClick={() => { onBackClick(null); }}>New User?</button>
     </form>
+=======
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    /* Send a request to the server for authentication */
+    axios.post('https://listapeli.herokuapp.com/login', {
+      Username: username,
+      Password: password
+    })
+      .then(response => {
+        const data = response.data;
+        props.onLoggedIn(data);
+      })
+      .catch(e => {
+        console.log('no such user')
+      });
+  };
+
+
+
+  return (
+    <Form>
+      <Form.Group controlId="formUsername">
+        <Form.Label>Username:</Form.Label>
+        <Form.Control type="text" placeholder="Enter username" value={username} onChange={e => setUsername(e.target.value)} />
+      </Form.Group>
+
+      <Form.Group controlId="formPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+      </Form.Group>
+      <Button variant="primary" type="submit" onClick={handleSubmit}>
+        Submit
+      </Button>
+    </Form>
+>>>>>>> Stashed changes
   );
 
 
 }
 
+<<<<<<< Updated upstream
+=======
+
+
+
+
+
+>>>>>>> Stashed changes
