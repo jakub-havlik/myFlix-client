33b106f91e3b4fb541fe139985609114b8fb255e
jakub-havlik <havlik9@gmail.com>
2022-04-15 10:02:31 +0200

    updated registration-view.jsx

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,61 +1,4 @@
 import React, { useState } from 'react';
-<<<<<<< Updated upstream
-
-export function RegistrationView(props) {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [email, setEmail] = useState('');
-  const [birthday, setBirthday] = useState('');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(username, password, email, birthday);
-    /* Send a request to the server for authentication */
-    /* then call props.onLoggedIn(username) */
-    props.onRegistration(username);
-  };
-
-
-  return (
-    <form>
-      <p>
-        <label>
-          Username:
-          <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-        </label>
-      </p>
-      <p>
-        <label>
-          Password:
-          <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-        </label>
-      </p>
-      <p>
-        <label>
-          Repeat Password:
-          <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-        </label>
-      </p>
-      <p>
-        <label>
-          Email:
-          <input type="email" value={password} onChange={e => setEmail(e.target.value)} />
-        </label>
-      </p>
-      <p>
-        <label>
-          Birthday:
-          <input type="birthday" value={password} onChange={e => setBirthday(e.target.value)} />
-        </label>
-      </p>
-
-      <button type="submit" onClick={handleSubmit}>Register</button>
-    </form>
-  );
-
-
-}
-=======
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
@@ -127,4 +70,3 @@ export function LoginView(props) {
     </Form>
   )
 }
->>>>>>> Stashed changes
