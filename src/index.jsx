8aa43628bb429b1fc1bdff01cb912a1937eb8a98
jakub-havlik <havlik9@gmail.com>
2022-04-15 09:44:12 +0200

    updated index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { MainView } from './components/main-view/main-view';
+<<<<<<< Updated upstream
+=======
+import Container from 'react-bootstrap/Container';
+>>>>>>> Stashed changes
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
@@ -9,7 +13,13 @@ import './index.scss';
 class MyFlixApplication extends React.Component {
   render() {
     return (
+<<<<<<< Updated upstream
       <MainView />
+=======
+      <Container>
+        <MainView />
+      </Container>
+>>>>>>> Stashed changes
     );
   }
 }
