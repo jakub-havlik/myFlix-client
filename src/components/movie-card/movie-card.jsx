d8b447975667e78c1433932639e38f7f34fe279f
jakub-havlik <havlik9@gmail.com>
2022-04-18 13:31:08 +0200

    updated movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,7 +1,7 @@
 // movie-card is a child component of main-view
 
 import React from 'react';
-import PropTypes from 'prop-types';
+import propTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
