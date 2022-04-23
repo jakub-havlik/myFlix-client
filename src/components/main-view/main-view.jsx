41fb85b6896df424b7d18faf11ba7a3039fa73ca
jakub-havlik <havlik9@gmail.com>
2022-04-23 19:13:16 +0200

    updated main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -7,8 +7,8 @@ import { RegistrationView } from '../registration-view/registration-view';
 import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+
+import { Container, Row, Col, Navbar } from 'react-bootstrap';
 
 
 export class MainView extends React.Component {
@@ -24,15 +24,16 @@ export class MainView extends React.Component {
   }
 
 
-
   getMovies(token) {
     axios.get('https://listapeli.herokuapp.com/movies', {
       headers: { Authorization: `Bearer ${token}` }
     })
       .then(response => {
-        // Assign the result to the state
+
+        const movies = response.data.filter(({ Title, Description, ImagePath }) => Title && Description && ImagePath)
+
         this.setState({
-          movies: response.data
+          movies
         });
       })
       .catch(function (error) {
@@ -84,7 +85,6 @@ export class MainView extends React.Component {
   }
 
 
-
   render() {
     const { movies, selectedMovie, user } = this.state;
 
@@ -95,25 +95,39 @@ export class MainView extends React.Component {
     if (movies.length === 0) return <div className="main-view" />;
 
     return (
-      <Row className="main-view justify-content-md-center">
-        <button onClick={() => { this.onLoggedOut() }}>Logout</button>
-        {selectedMovie
-          ? (
-            <Col md={8}>
-              <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
-            </Col>
-          )
-          : movies.map(movie => (
-            <Col md={3}>
-              <MovieCard key={movie._id} movieData={movie} onMovieClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
-            </Col>
-          ))
-        }
-      </Row>
+      <>
+        <Navbar bg="dark" variant="dark" className="mb-5">
+          <Container>
+            <Navbar.Brand href="#">list[a]peli</Navbar.Brand>
+            <Navbar.Toggle />
+            <Navbar.Collapse className="justify-content-end">
+              <Navbar.Text onClick={() => { this.onLoggedOut() }} style={{
+                cursor: 'pointer'
+              }}>
+                Logout
+              </Navbar.Text>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <Row className="main-view justify-content-md-center">
+          {selectedMovie
+            ? (
+              <Col md={8}>
+                <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+              </Col>
+            )
+            : movies.map(movie => (
+              <Col md={4} key={movie._id} >
+                <MovieCard movieData={movie} onMovieClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+              </Col>
+            ))
+          }
+        </Row>
+      </>
     );
 
-
   }
+
 
 
 }
