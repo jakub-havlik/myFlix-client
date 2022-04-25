c84bff0c4ec6b8135ca697b20107ec94586c6efe
jakub-havlik <havlik9@gmail.com>
2022-04-25 08:28:42 +0200

    updated main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,14 +3,6 @@
 import React from 'react';
 import axios from 'axios';
 
-<<<<<<< Updated upstream
-import { RegistrationView } from '../registration-view/registration-view';
-import { LoginView } from '../login-view/login-view';
-import { MovieCard } from '../movie-card/movie-card';
-import { MovieView } from '../movie-view/movie-view';
-
-import { Container, Row, Col, Navbar } from 'react-bootstrap';
-=======
 import { BrowserRouter as Router, Route, Routes, Redirect } from "react-router-dom";
 
 import { Navbar } from '../navbar/navbar';
@@ -25,51 +17,21 @@ import { UpdateUser } from '../profile-view/update-user';
 import { Container, Row, Col } from 'react-bootstrap';
 
 import './main-view.scss';
->>>>>>> Stashed changes
 
 
 export class MainView extends React.Component {
 
   constructor() {
     super();
-<<<<<<< Updated upstream
-    // Initial state is set to null
-    this.state = {
-      movies: [],
-      selectedMovie: null,
-=======
     this.state = {
       // Creating an empty array to hold movie data from database
       movies: [],
       // Set initial user state to null, used for user login --> Default is logged out
->>>>>>> Stashed changes
       user: null
     };
   }
 
-<<<<<<< Updated upstream
-
-  getMovies(token) {
-    axios.get('https://listapeli.herokuapp.com/movies', {
-      headers: { Authorization: `Bearer ${token}` }
-    })
-      .then(response => {
-
-        const movies = response.data.filter(({ Title, Description, ImagePath }) => Title && Description && ImagePath)
-
-        this.setState({
-          movies
-        });
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
-
-
-=======
   // When token is present (user is logged in), get list of movies
->>>>>>> Stashed changes
   componentDidMount() {
     let accessToken = localStorage.getItem('token');
     if (accessToken !== null) {
@@ -80,19 +42,6 @@ export class MainView extends React.Component {
     }
   }
 
-<<<<<<< Updated upstream
-
-
-  /*When a movie is clicked, this function is invoked and updates the state of the `selectedMovie` *property to that movie*/
-
-  setSelectedMovie(movie) {
-    this.setState({
-      selectedMovie: movie
-    });
-  }
-
-=======
->>>>>>> Stashed changes
   /* When a user successfully logs in, this function updates the `user` property in state to that *particular user*/
 
   onLoggedIn(authData) {
@@ -107,58 +56,6 @@ export class MainView extends React.Component {
   }
 
 
-<<<<<<< Updated upstream
-  onLoggedOut() {
-    localStorage.removeItem('token');
-    localStorage.removeItem('user');
-    this.setState({
-      user: null
-    });
-  }
-
-
-  render() {
-    const { movies, selectedMovie, user } = this.state;
-
-    /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
-    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
-
-    // Before the movies have been loaded
-    if (movies.length === 0) return <div className="main-view" />;
-
-    return (
-      <>
-        <Navbar bg="dark" variant="dark" className="mb-5">
-          <Container>
-            <Navbar.Brand href="#">list[a]peli</Navbar.Brand>
-            <Navbar.Toggle />
-            <Navbar.Collapse className="justify-content-end">
-              <Navbar.Text onClick={() => { this.onLoggedOut() }} style={{
-                cursor: 'pointer'
-              }}>
-                Logout
-              </Navbar.Text>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-        <Row className="main-view justify-content-md-center">
-          {selectedMovie
-            ? (
-              <Col md={8}>
-                <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
-              </Col>
-            )
-            : movies.map(movie => (
-              <Col md={4} key={movie._id} >
-                <MovieCard movieData={movie} onMovieClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
-              </Col>
-            ))
-          }
-        </Row>
-      </>
-    );
-
-=======
   getMovies(token) {
     axios.get('https://listapeli.herokuapp.com/movies', {
       headers: { Authorization: `Bearer ${token}` }
@@ -262,15 +159,10 @@ export class MainView extends React.Component {
     );
 
 
->>>>>>> Stashed changes
   }
 
 
 
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
 }
 
 
@@ -286,9 +178,6 @@ export class MainView extends React.Component {
 
 
 
-<<<<<<< Updated upstream
-=======
 
 
 
->>>>>>> Stashed changes
