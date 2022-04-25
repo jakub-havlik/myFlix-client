3198c26f6e9f8639e22eff3b90786d1b1383b47d
jakub-havlik <havlik9@gmail.com>
2022-04-25 08:15:41 +0200

    updated main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,26 +3,51 @@
 import React from 'react';
 import axios from 'axios';
 
+<<<<<<< Updated upstream
 import { RegistrationView } from '../registration-view/registration-view';
 import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
 import { Container, Row, Col, Navbar } from 'react-bootstrap';
+=======
+import { BrowserRouter as Router, Route, Routes, Redirect } from "react-router-dom";
+
+import { Navbar } from '../navbar/navbar';
+import { LoginView } from '../login-view/login-view';
+import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
+import { RegistrationView } from '../registration-view/registration-view';
+import { DirectorView } from '../director-view/director-view';
+import { ProfileView } from '../profile-view/profile-view';
+import { UpdateUser } from '../profile-view/update-user';
+
+import { Container, Row, Col } from 'react-bootstrap';
+
+import './main-view.scss';
+>>>>>>> Stashed changes
 
 
 export class MainView extends React.Component {
 
   constructor() {
     super();
+<<<<<<< Updated upstream
     // Initial state is set to null
     this.state = {
       movies: [],
       selectedMovie: null,
+=======
+    this.state = {
+      // Creating an empty array to hold movie data from database
+      movies: [],
+      // Set initial user state to null, used for user login --> Default is logged out
+>>>>>>> Stashed changes
       user: null
     };
   }
 
+<<<<<<< Updated upstream
 
   getMovies(token) {
     axios.get('https://listapeli.herokuapp.com/movies', {
@@ -42,6 +67,9 @@ export class MainView extends React.Component {
   }
 
 
+=======
+  // When token is present (user is logged in), get list of movies
+>>>>>>> Stashed changes
   componentDidMount() {
     let accessToken = localStorage.getItem('token');
     if (accessToken !== null) {
@@ -52,6 +80,7 @@ export class MainView extends React.Component {
     }
   }
 
+<<<<<<< Updated upstream
 
 
   /*When a movie is clicked, this function is invoked and updates the state of the `selectedMovie` *property to that movie*/
@@ -62,6 +91,8 @@ export class MainView extends React.Component {
     });
   }
 
+=======
+>>>>>>> Stashed changes
   /* When a user successfully logs in, this function updates the `user` property in state to that *particular user*/
 
   onLoggedIn(authData) {
@@ -76,6 +107,7 @@ export class MainView extends React.Component {
   }
 
 
+<<<<<<< Updated upstream
   onLoggedOut() {
     localStorage.removeItem('token');
     localStorage.removeItem('user');
@@ -126,10 +158,119 @@ export class MainView extends React.Component {
       </>
     );
 
+=======
+  getMovies(token) {
+    axios.get('https://listapeli.herokuapp.com/movies', {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+      .then(response => {
+        // Assign the result to the state
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
 
 
+  render() {
+    const { movies, user } = this.state;
+
+
+
+    return (
+      <Router>
+        < Navbar user={user} />
+        <Container>
+
+          <Row className="main-view justify-content-md-center">
+            <Routes>
+
+              <Route exact path="/" render={() => {
+                /* If there is no user, the LoginView is rendered */
+                /* If there is a user logged in, the user details are *passed as a prop to the LoginView */
+                if (!user) return (
+                  <Col md={6}>
+                    <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+                  </Col>
+                )
+
+                // If movie list is empty (while movies load from API), display empty page
+                if (movies.length === 0) return <div className="main-view" />;
+
+                return movies.map(m => (
+                  <Col xs={12} sm={6} md={4} lg={3} className="d-flex" key={m._id}>
+                    <MovieCard movie={m} />
+                  </Col>
+
+                ))
+              }} />
+
+              <Route path="/register" render={() => {
+                if (user) return <Redirect to="/" />
+                return (
+                  <Col xs={12} md={8}>
+                    <RegistrationView />
+                  </Col>
+                )
+              }} />
+              <Route path="/movies/:movieId" render={({ match, history }) => {
+                return (
+                  <Col xs={12} md={10}>
+                    <MovieView movie={movies.find(m => m._id === match.params.movieId)} onBackClick={() => history.goBack()} />
+                  </Col>
+                )
+              }} />
+              <Route path={"/users/${user}"} render={({ history }) => {
+                if (!user) return <Redirect to="/" />
+                return (
+                  <Col xs={12} md={10}>
+                    <ProfileView user={user} movies={movies} onBackClick={() => history.goBack()} />
+                  </Col>
+                )
+              }} />
+              <Route path={"/directors/:name"} render={({ match, history }) => {
+                if (!user) return <Redirect to="/" />
+                // If movie list is empty (while movies load from API), display empty page
+                if (movies.length === 0) return <div className="main-view" />;
+                return (
+                  <Col xs={12} md={10}>
+                    <DirectorView movies={movies} director={movies.find(m => m.Director.Name === match.params.name).Director} onBackClick={() => history.goBack()} />
+                  </Col>
+                )
+              }} />
+              <Route path={"/genres/:name"} render={({ match, history }) => {
+                if (!user) return <Redirect to="/" />
+                // If movie list is empty (while movies load from API), display empty page
+                if (movies.length === 0) return <div className="main-view" />;
+                return (
+                  <Col xs={12} md={10}>
+                    <GenreView movies={movies} genre={movies.find(m => m.Genre.Name === match.params.name).Genre} onBackClick={() => history.goBack()} />
+                  </Col>
+                )
+              }} />
+
+
+            </Routes>
+          </Row>
+        </Container>
+      </Router>
+
+    );
+
+
+>>>>>>> Stashed changes
+  }
+
+
+
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 }
 
 
@@ -145,3 +286,9 @@ export class MainView extends React.Component {
 
 
 
+<<<<<<< Updated upstream
+=======
+
+
+
+>>>>>>> Stashed changes
