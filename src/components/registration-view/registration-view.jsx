516c1ca6e3fa38a6976c30e52165e5ab6e7c9148
jakub-havlik <havlik9@gmail.com>
2022-04-25 08:29:58 +0200

    updated registration-view.jsx

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,28 +1,4 @@
 import React, { useState } from 'react';
-<<<<<<< Updated upstream
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-
-export function LoginView(props) {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  // Declare hook for each input
-  const [usernameErr, setUsernameErr] = useState('');
-  const [passwordErr, setPasswordErr] = useState('');
-
-  // validate user inputs
-  const validate = () => {
-    let isReq = true;
-    if (!username) {
-      setUsernameErr('Username Required');
-      isReq = false;
-    } else if (username.length < 2) {
-      setUsernameErr('Username must be 2 characters long');
-      isReq = false;
-    }
-    if (!password) {
-      setPasswordErr('Password Required');
-=======
 import axios from 'axios';
 
 import { Link } from 'react-router-dom';
@@ -57,57 +33,12 @@ export function RegistrationView(props) {
 
     if (!password) {
       setPasswordErr('Password is Required');
->>>>>>> Stashed changes
       isReq = false;
     } else if (password.length < 6) {
       setPassword('Password must be 6 characters long');
       isReq = false;
     }
 
-<<<<<<< Updated upstream
-    return isReq;
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const isReq = validate();
-    if (isReq) {
-      /* Send request to the server for authentication */
-      axios.post('https://listapeli.herokuapp.com/login', {
-        Username: username,
-        Password: password
-      })
-        .then(response => {
-          const data = response.data;
-          props.onLoggedIn(data);
-        })
-        .catch(e => {
-          console.log('no such user')
-        });
-    }
-  };
-
-  return (
-    <Form>
-      <Form.Group controlId="formUsername">
-        <Form.Label>Username:</Form.Label>
-        <Form.Control type="text" placeholder="Enter username" value={username} onChange={e => setUsername(e.target.value)} />
-        {/* code added here to display validation error */}
-        {usernameErr && <p>{usernameErr}</p>}
-      </Form.Group>
-
-      <Form.Group controlId="formPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
-        {/* code added here to display validation error */}
-        {passwordErr && <p>{passwordErr}</p>}
-      </Form.Group>
-      <Button variant="primary" type="submit" onClick={handleSubmit}>
-        Submit
-      </Button>
-    </Form>
-  )
-=======
 
     if (!email) {
       setEmailErr('Email is required!');
@@ -189,5 +120,4 @@ export function RegistrationView(props) {
     </>
   );
 
->>>>>>> Stashed changes
 }
