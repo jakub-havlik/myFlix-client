3b804b01e6cd9ffadfc6908604430d3bb5f91a21
jakub-havlik <havlik9@gmail.com>
2022-05-05 12:46:56 +0200

    updated login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -6,21 +6,25 @@ import Button from 'react-bootstrap/Button';
 export function LoginView(props) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
 
   const handleSubmit = (e) => {
     e.preventDefault();
     /* Send a request to the server for authentication */
+    setIsLoading(true);
     axios.post('https://listapeli.herokuapp.com/login', {
       Username: username,
       Password: password
     })
       .then(response => {
         const data = response.data;
+        setIsLoading(false);
         props.onLoggedIn(data);
       })
       .catch(e => {
-        console.log('no such user')
+        console.log(e)
+        setIsLoading(false);
       });
   };
 
@@ -30,15 +34,16 @@ export function LoginView(props) {
     <Form className="m-5">
       <Form.Group className="mb-3" controlId="formUsername">
         <Form.Label>Username:</Form.Label>
-        <Form.Control type="text" placeholder="Enter username" value={username} onChange={e => setUsername(e.target.value)} />
+        <Form.Control type="text" disabled={isLoading} placeholder="Enter username" value={username} onChange={e => setUsername(e.target.value)} />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+        <Form.Control disabled={isLoading} type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
       </Form.Group>
-      <Button variant="primary" type="submit" onClick={handleSubmit}>
-        Submit
+      <Button variant="primary" type="submit" onClick={handleSubmit} disabled={isLoading}>
+        {isLoading ? <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> : ''}
+        {isLoading ? ' Loading...' : 'Submit'}
       </Button>
     </Form>
   );
