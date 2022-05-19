142e8e2ab9e140868f4ce1d4cbcc8cb0cf5291a4
jakub-havlik <havlik9@gmail.com>
2022-05-19 11:51:48 +0200

    updated genre-view.jsx

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -1,48 +1,52 @@
-import React from 'react';
+import React from "react";
 
-import { MovieCard } from '../movie-card/movie-card';
+import { MovieCard } from "../movie-card/movie-card";
 
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export function GenreView(props) {
+  //console.log(props);
   return (
     <>
-
       <div>
-        <Button variant="outline-light" onClick={() => { props.onBackClick() }}>Back</Button>
+        <Button
+          variant="outline-light"
+          onClick={() => {
+            props.onBackClick();
+          }}
+        >
+          Back
+        </Button>
       </div>
 
       <div>
-        <h1 className="display-4">{props.genre.Name}</h1>
+        <h1 className="display-4">{props.genre.Name.join(",")}</h1>
       </div>
       <div>
-        <span className="value">{props.genre.Description}</span>
+        <span className="value">{props.genre.Description.join(",")}</span>
       </div>
       <br />
       <div>
         <h4>Some movies in this genre:</h4>
       </div>
 
-
-
       <Row className="justify-content-md-center">
-        {props.movies.filter(m => m.Genre.Name === props.genre.Name).map(m => (
-          <Col xs={12} sm={6} md={4} className="d-flex" key={m._id}>
-            <MovieCard movie={m} />
-          </Col>
-        ))}
+        {props.movies
+          .filter((m) => m.Genre.Name === props.genre.Name)
+          .map((m) => (
+            <Col xs={12} sm={6} md={4} className="d-flex" key={m._id}>
+              <MovieCard movie={m} />
+            </Col>
+          ))}
       </Row>
-
-
-
 
       <Link to={"/"}>
         <Button variant="outline-light">Back to full list</Button>
       </Link>
     </>
-  )
+  );
 }
