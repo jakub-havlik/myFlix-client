2fac6031d3a47223c96cb5eee70705c6ad1895d4
jakub-havlik <havlik9@gmail.com>
2022-05-23 13:27:57 +0200

    updated movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,12 +1,16 @@
 // movie-card is a child component of main-view
 
 import React from 'react';
+<<<<<<< Updated upstream
 import propTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 import { Link } from "react-router-dom";
 
+=======
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
 
 export class MovieCard extends React.Component {
   render() {
@@ -29,13 +33,36 @@ export class MovieCard extends React.Component {
     );
   }
 }
+<<<<<<< Updated upstream
 
 
+=======
+=======
+import propTypes from 'prop-types';
+import Card from 'react-bootstrap/Card';
+import { Link } from "react-router-dom";
+
+
+export class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    const { Title, Description, ImagePath, _id } = movie;
+    return (
+      <Card className="mb-4">
+        <Link to={`/movie/${_id}`}>
+          <Card.Img loading="lazy" variant="top" title={Title} src={ImagePath} width="400" alt={Title} />
+        </Link>
+      </Card>
+    );
+  }
+}
+>>>>>>> Stashed changes
 
 
 MovieCard.propTypes = {
   movie: propTypes.shape({
     Title: propTypes.string.isRequired,
+<<<<<<< Updated upstream
     Description: propTypes.string.isRequired,
     ImagePath: propTypes.string.isRequired
   }).isRequired,
@@ -59,3 +86,19 @@ MovieCard.propTypes = {
 
 
 
+=======
+    ReleaseYear: propTypes.string.isRequired,
+    Description: propTypes.string.isRequired,
+    Director: propTypes.shape({
+      Name: propTypes.string.isRequired,
+    }).isRequired,
+    Actors: propTypes.array.isRequired,
+    Genre: propTypes.shape({
+      Name: propTypes.array.isRequired,
+    }).isRequired,
+    ImagePath: propTypes.string.isRequired
+  }).isRequired
+};
+
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
