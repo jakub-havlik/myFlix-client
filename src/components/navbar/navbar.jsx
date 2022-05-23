7ec72d67637d400e23a34c2bd3529dda6b5b2635
jakub-havlik <havlik9@gmail.com>
2022-05-23 13:27:20 +0200

    updated navbar.jsx

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -22,14 +22,28 @@ export function Navbar({ user }) {
   };
 
   return (
+<<<<<<< Updated upstream
     <Navbar className="main-nav" sticky="top" bg="dark" expand="lg" variant="dark">
       <Container fluid>
         <Navbar.Brand className="navbar-logo" href="/">list[a]peli</Navbar.Brand>
+=======
+    <Navbar className="main-nav" sticky="top" expand="lg" variant="dark">
+      <Container fluid style={{ margin: 0, padding: 0 }}>
+        <Link to="/">
+          <Navbar.Brand className="navbar-logo">list[a]peli</Navbar.Brand>
+        </Link>
+>>>>>>> Stashed changes
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ml-auto">
             {isAuth() && (
+<<<<<<< Updated upstream
               <Nav.Link id="nav-link" href="/profile">{user}</Nav.Link>
+=======
+              <Link to="/profile" className="nav-link">
+                {user}
+              </Link>
+>>>>>>> Stashed changes
             )}
             {isAuth() && (
               <Button variant="link" label="Logout" onClick={() => {
@@ -37,11 +51,21 @@ export function Navbar({ user }) {
               }}>Log out</Button>
             )}
             {!isAuth() && (
+<<<<<<< Updated upstream
               <Nav.Link href="/register">Register</Nav.Link>
+=======
+              <Link className="nav-link" to="/register">
+                Register
+              </Link>
+>>>>>>> Stashed changes
             )}
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 }
