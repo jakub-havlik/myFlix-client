3914dcae65cef4b0879d09f4396fd2f4d181e9be
jakub-havlik <havlik9@gmail.com>
2022-05-23 13:55:55 +0200

    updated director-view.jsx

---
+++ src/components/director-view/director-view.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+import { MovieCard } from '../movie-card/movie-card';
+
+
+export function DirectorView(props) {
+  return (
+    <div className="director-view" style={{ color: 'white' }}>
+
+      <div>
+        <h1 className="display-4">{props.director.Name}</h1>
+      </div>
+      <div>
+        <span className="value">*{props.director.Birth}</span>
+      </div>
+      <div>
+        <span className="value">{props.director.Bio}</span>
+      </div>
+      <br />
+      <div>
+        <h4>other movies by {props.director.Name}:</h4>
+      </div>
+
+      <Row className="justify-content-md-center">
+        {props.movies.filter(m => m.Director.Name === props.director.Name).map(m => (
+          <Col xs={12} sm={6} md={4} className="d-flex" key={m._id}>
+            <MovieCard movie={m} />
+          </Col>
+        ))}
+      </Row>
+
+      <Link to={"/"}>
+        <Button variant="outline-light">Back to full list</Button>
+      </Link>
+
+    </div>
+  )
+}
