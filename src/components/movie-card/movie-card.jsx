f5da22d5a0f9d6bad65db8cefc49a27b26937958
jakub-havlik <havlik9@gmail.com>
2022-05-24 08:52:40 +0200

    updated movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,43 +1,4 @@
-// movie-card is a child component of main-view
-
 import React from 'react';
-<<<<<<< Updated upstream
-import propTypes from 'prop-types';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-
-import { Link } from "react-router-dom";
-
-=======
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-
-export class MovieCard extends React.Component {
-  render() {
-    // extracting the prop
-    // movieData is the prop name
-    //const { movieData } = this.props;
-    const { movie } = this.props;
-
-    return (
-      <Card>
-        <Card.Img variant="top" src={movie.ImagePath} />
-        <Card.Body>
-          <Card.Title>{movie.Title}</Card.Title>
-          <Card.Text>{movie.Description}</Card.Text>
-          <Link to={`/movies/${movie._id}`}>
-            <Button variant="link">Open</Button>
-          </Link>
-        </Card.Body>
-      </Card>
-    );
-  }
-}
-<<<<<<< Updated upstream
-
-
-=======
-=======
 import propTypes from 'prop-types';
 import Card from 'react-bootstrap/Card';
 import { Link } from "react-router-dom";
@@ -56,37 +17,11 @@ export class MovieCard extends React.Component {
     );
   }
 }
->>>>>>> Stashed changes
 
 
 MovieCard.propTypes = {
   movie: propTypes.shape({
     Title: propTypes.string.isRequired,
-<<<<<<< Updated upstream
-    Description: propTypes.string.isRequired,
-    ImagePath: propTypes.string.isRequired
-  }).isRequired,
-  onMovieClick: propTypes.func.isRequired
-};
-
-
-/*MovieCard.propTypes = {
-  movieData: propTypes.shape({
-    Title: propTypes.string.isRequired,
-    Description: propTypes.string.isRequired,
-    ImageURL: propTypes.string.isRequired,
-    ReleaseYear: propTypes.string.isRequired,
-    Genre: propTypes.shape({
-      Name: propTypes.array.isRequired
-    })
-  }).isRequired,
-  onMovieClick: propTypes.func.isRequired
-};*/
-
-
-
-
-=======
     ReleaseYear: propTypes.string.isRequired,
     Description: propTypes.string.isRequired,
     Director: propTypes.shape({
@@ -100,5 +35,3 @@ MovieCard.propTypes = {
   }).isRequired
 };
 
->>>>>>> Stashed changes
->>>>>>> Stashed changes
