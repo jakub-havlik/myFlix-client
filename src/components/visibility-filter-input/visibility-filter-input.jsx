d098ec6ab3daf97b2875a9b2e7057ce72eb4a31a
jakub-havlik <havlik9@gmail.com>
2022-06-30 17:50:52 +0200

    updated visibility-filter-input.jsx

--- src/components/visibility-filter-input/visibility-filter-input.jsx
+++ src/components/visibility-filter-input/visibility-filter-input.jsx
@@ -2,21 +2,24 @@
 // this component is a sub-component of MoviesList
 
 import React from 'react';
-import { connect } from 'react-redux';
-
 import Form from 'react-bootstrap/Form';
-
+// react redux
+import { connect } from 'react-redux';
 import { setFilter } from '../../actions/actions';
+// styling
+//import "./visibility-filter-input.scss";
+
 
 function VisibilityFilterInput(props) {
-    return <Form.Control
-        onChange={e => props.setFilter(e.target.value)}
-        value={props.visibilityFilter}
-        placeholder="filter"
-    />;
+  return <Form.Control
+    onChange={e => props.setFilter(e.target.value)}
+    value={props.visibilityFilter}
+    placeholder="filter"
+    type="text"
+  />;
 }
 
 export default connect(
-    null,
-    { setFilter }
+  null,
+  { setFilter }
 )(VisibilityFilterInput);
